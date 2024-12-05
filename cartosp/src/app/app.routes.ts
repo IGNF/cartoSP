b4fb8b9486a4c02d0806029eb0585724eab23585
cde-barros <Christopher.De-Barros@ign.fr>
2024-12-05 18:16:57 +0100

    suppression de l'ancien dossier projet cartosp

--- cartosp/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { CarteComponent } from './carte/carte.component';
-
-export const routes: Routes = [  {
-    path: '',
-    component: CarteComponent
-  }, 
-];
