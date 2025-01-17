b22fa31fa7e2ce0ca8fa4f0abbea27491749f8d1
cde-barros <Christopher.De-Barros@ign.fr>
2025-01-17 10:05:18 +0100

    ajout footer logo et classes style marker

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 import Map from 'ol/Map';
+import Feature from 'ol/Feature';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
 import { bbox as bboxStrategy } from 'ol/loadingstrategy';
 import { LayerWMTS as GeoportalLayerWMTS, LayerWFS as GeoportalLayerWFS } from "geopf-extensions-openlayers/src";
 
@@ -17,6 +20,68 @@ export class CarteComponent implements OnInit {
   constructor(private elementRef: ElementRef) {}
 
   ngOnInit() {
+
+    var styleCache = {
+      "Administration locale": new Style({
+        image: new Icon({
+          anchor: [0.5, 37],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'assets/images/mapmarker/administrations-locales/implantation.svg',
+        })
+      }),
+      "Droit, justice": new Style({
+        image: new Icon({
+          anchor: [0.5, 37],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'assets/images/mapmarker/droit-justice/implantation.svg',
+        })
+      }),
+      "Economie, finances, consommation" : new Style({
+        image: new Icon({
+          anchor: [0.5, 37],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'assets/images/mapmarker/finance/implantation.svg',
+        })
+      }),
+      "Enseignement, recherche" : new Style({
+        image: new Icon({
+          anchor: [0.5, 37],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'assets/images/mapmarker/droit-justice/implantation.svg',
+        })
+      }),
+      "Social, santé" : new Style({
+        image: new Icon({
+          anchor: [0.5, 37],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'assets/images/mapmarker/social-sante/implantation.svg',
+        })
+      }),
+
+      "Travail, emploi, formation": new Style({
+        image: new Icon({
+          anchor: [0.5, 37],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'assets/images/mapmarker/emploi-formation/implantation.svg',
+        })
+      }),
+
+      "Environnement, logement, transports": new Style({
+        image: new Icon({
+          anchor: [0.5, 37],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'assets/images/mapmarker/environnement/implantation.svg',
+        })
+      })
+    } as any;
+
     this.map.setLayers([
       new GeoportalLayerWMTS({
         layer: "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
@@ -27,6 +92,10 @@ export class CarteComponent implements OnInit {
       new GeoportalLayerWFS({
         layer: "mvp_carto20241219_bis_gpkg_20-12-2024_wfs:mvp_carto20241219_bis",
         olParams : {
+          minZoom: 10,
+          style: function(feature: Feature){
+            return styleCache[feature.getProperties()['thematique']];
+          },
           sourceParams: {
             strategy: bboxStrategy,
           }
