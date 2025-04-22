7d533c27b33b68ee593dbbaa36323f36bdf61d29
cde-barros <Christopher.De-Barros@ign.fr>
2025-04-22 14:11:53 +0200

    ajout du nouveau wfs filter

--- src/app/controls/spwfsfilter/spwfsfilter.component.ts
+++ src/app/controls/spwfsfilter/spwfsfilter.component.ts
@@ -23,47 +23,35 @@ export class SpwfsfilterComponent implements OnInit {
         "Administration locale": {
             markerPath: "assets/images/mapmarker/administrations-locales/",
             topologies: [
-              "France services",
+                "Conseil départemental",
+                "France services",
                 "Mairie",
+                "Maison départementale des solidarités (MDS)",
                 "Point d'accueil numérique (Préfecture et Sous-préfecture)",
                 "Préfecture",
-                "Sous-préfecture"
-            ]
-        },
-        "Droit, justice": {
-            markerPath: "assets/images/mapmarker/droit-justice/",
-            topologies: [
-                "Conseil départemental d'accès au droit (CDAD)",
-                "Point-justice - France services ou relais d'accès au droit",
-                "Point-justice - Maison de la justice et du droit",
-                "Tribunal judiciaire"
-            ]
-        },
-        "Economie, finances, consommation" : {
-            markerPath: "assets/images/mapmarker/finance/",
-            topologies: [
-                "Trésorerie",
-                "Service des impôts des entreprises (SIE)",
-                "Service des impôts des particuliers (SIP)"
+                "Sous-préfecture",
             ]
         },
         "Social, santé" : {
             markerPath: "assets/images/mapmarker/social-sante/",
             topologies: [
-                "Maison de santé (L.6223-3)",
-                "Service d’aide médicale urgente (Samu)",
-                "Centre de Santé",
-                "Centres Locaux Information Coordination P.A .(C.L.I.C.)",
+                "Caisse commune de Sécurité sociale (CCSS)",
                 "Caisse d'allocations familiales (Caf)",
                 "Caisse d'assurance retraite et de la santé au travail (Carsat)",
-                "Caisse primaire d’assurance maladie (CPAM)",
+                "Caisse primaire d'assurance maladie (CPAM)",
+                "Centre communal d'action sociale (CCAS)",
+                "Centre de Santé",
                 "Centre Hospitalier (C.H.)",
                 "Centre Hospitalier Régional (C.H.R.)",
                 "Centre hospitalier universitaire (CHU)",
+                "Centres Locaux Information Coordination P.A .(C.L.I.C.)",
                 "Centres sociaux",
+                "Communautés professionnelles territoriales de santé (CPTS)",
                 "Maison départementale des personnes handicapées (MDPH)",
-                "Mutualité sociale agricole (MSA), réseau local",
-                "Union de recouvrement des cotisations de sécurité sociale et d’allocations familiales (Urssaf)"                    
+                "Maisons des solidarités et antennes",
+                "Mutualité sociale agricole (MSA)",
+                "Service d’aide médicale urgente (Samu)",
+                "Union de recouvrement des cotisations de sécurité sociale et d’allocations familiales (Urssaf)"                  
             ]
         },
         "Travail, emploi, formation": {
@@ -73,15 +61,41 @@ export class SpwfsfilterComponent implements OnInit {
                 "Mission locale pour l'insertion professionnelle et sociale des jeunes (16-25 ans)"
             ]
         },
+        "Economie, finances, consommation" : {
+            markerPath: "assets/images/mapmarker/finance/",
+            topologies: [
+                "Direction départementale des finances publiques (DDFIP)",
+                "Service des impôts des entreprises (SIE)",
+                "Service des impôts des particuliers (SIP)",
+                "Trésorerie"
+            ]
+        },
+        "Droit, justice": {
+            markerPath: "assets/images/mapmarker/droit-justice/",
+            topologies: [
+                "Conseil départemental d'accès au droit (CDAD)",
+                "Point-justice",
+                "Tribunal judiciaire"
+            ]
+        },
         "Environnement, logement, transports":  {
             markerPath: "assets/images/mapmarker/environnement/",
             topologies: [
                 "Agence départementale d'information sur le logement (Adil)",
-                "Agence nationale de l’habitat (ANAH) - réseau local",
+                "Agence nationale de l'habitat (ANAH)",
                 "Bureau d'aide aux victimes du tribunal judiciaire",
                 "Espace conseil France rénov'"
             ]
-    }};
+        },
+        "Sécurité, défense":  {
+            markerPath: "assets/images/mapmarker/securite/",
+            topologies: [
+                "Brigade de gendarmerie",
+                "Commissariat de police",
+                "Police municipale"
+            ]
+        }
+    };
 
     this.control = new WfsFilter({
       position: "top-left",
