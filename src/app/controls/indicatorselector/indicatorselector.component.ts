c04533343bd7189acb5c3c8b91ea97330a32bfb9
cde-barros <Christopher.De-Barros@ign.fr>
2026-05-21 17:53:00 +0200

    ajout des nouveaux indicateurs

--- src/app/controls/indicatorselector/indicatorselector.component.ts
+++ src/app/controls/indicatorselector/indicatorselector.component.ts
@@ -21,13 +21,6 @@ export class IndicatorselectorComponent implements OnInit {
       // Liste des indicateurs
       var indicateurs = [
         {
-            thematique: "Services publics",
-            indicators: [
-                {title:"Fréquentation des implantations", layername: "frequentation_SP_v3", opacity: 0.7},
-                {title:"Fréquentation des permanences", layername: "frequentation_SP_permanences", opacity: 0.7},
-            ]
-        },
-        {
             thematique: "Démographie",
             indicators: [
                 {title:"Densité de population", layername: "IGNF_CARTO-SP_DENSITE-POPULATION-CARREAU__VALID", opacity: 1},
@@ -39,37 +32,37 @@ export class IndicatorselectorComponent implements OnInit {
             thematique: "Précarité",
             indicators: [
                 {title:"Part des ménages pauvres", layername: "IGNF_CARTO-SP_PART-MENAGES-PAUVRES__VALID", opacity: 1},
-                {title:"Niveau de vie moyen", layername: "total_pilotes_insee_niveau_de_vie", opacity: 1},
+                //{title:"Niveau de vie moyen", layername: "total_pilotes_insee_niveau_de_vie", opacity: 1},
             ]
         },
         {
             thematique: "Fragilité numérique",
             indicators: [
-                {title:"Indicateur de fragilité numérique", layername: "total_pilotes_mednum_fragilite_numerique_2", opacity: 1},
+                {title:"Indicateur de fragilité numérique", layername: "IGNF_CARTO-SP_INDICATEUR-FRAGILITE-NUMERIQUE__VALID", opacity: 1},
             ]
         },
         {
             thematique: "Zonage",
             indicators: [
-                {title:"Bassin de vie", layername: "bassins_de_vie_france", opacity: 1},
-                {title:"Quartiers prioritaires de la ville", layername: "ANCT_QPV_wms", opacity: 1},
+                {title:"Bassin de vie", layername: "IGNF_CARTO-SP_BASSINS-VIE__VALID", opacity: 1},
+                //{title:"Quartiers prioritaires de la ville", layername: "ANCT_QPV_wms", opacity: 1},
             ]
         },
         {
             thematique: "Santé et accès aux soins",
             indicators: [
-                {title:"Professionnels de premier recours", layername: ""}, // sous-theme
-                {title:"Médecins généralistes", layername: "total_sante_nb_generalistes", opacity: 1},
-                {title:"Infirmiers", layername: "total_sante_nb_infirmiers", opacity: 1},
-                {title:"Kinésithérapeutes", layername: "total_sante_nb_kines", opacity: 1},
-                {title:"Chirurgiens-dentistes", layername: "total_sante_nb_dentistes", opacity: 1},
-                {title:"Orthophonistes", layername: "total_sante_nb_orthophonistes", opacity: 1},
-                {title:"Sages-femmes", layername: "total_sante_nb_sagesfemmes", opacity: 1},
+                // {title:"Professionnels de premier recours", layername: ""}, // sous-theme
+                {title:"Médecins généralistes", layername: "IGNF_CARTO-SP_PPR_MEDECINS__VALID", opacity: 1},
+                {title:"Infirmiers", layername: "IGNF_CARTO-SP_PPR_INFIRMIERS__VALID", opacity: 1},
+                {title:"Kinésithérapeutes", layername: "IGNF_CARTO-SP_PPR_KINESITHERAPEUTES__VALID", opacity: 1},
+                {title:"Chirurgiens-dentistes", layername: "IGNF_CARTO-SP_PPR_DENTISTES__VALID", opacity: 1},
+                {title:"Orthophonistes", layername: "IGNF_CARTO-SP_PPR_ORTHOPHONISTES__VALID", opacity: 1},
+                {title:"Sages-femmes", layername: "IGNF_CARTO-SP_PPR_SAGES_FEMMES__VALID", opacity: 1},
                 {title:"Territoire", layername: ""}, // sous-theme
-                {title:"Densité médicale", layername: "total_sante_densite_medicale", opacity: 1},
-                {title:"Moyenne d'âge des médecins", layername: "total_sante_moyenne_age_medecin", opacity: 1},
-                {title:"Accessibilité aux soins de premier recours", layername: "total_sante_accessibilite", opacity: 1},
-                {title:"Zonage CPTS", layername: "cpts_20250513", opacity: 1},
+                {title:"Densité médicale", layername: "IGNF_CARTO-SP_DENSITE-MEDECINS__VALID", opacity: 1},
+                {title:"Moyenne d'âge des médecins", layername: "IGNF_CARTO-SP_MOYENNE-AGE-MEDECINS__VALID", opacity: 1},
+                {title:"Accessibilité aux soins de premier recours", layername: "IGNF_CARTO-SP_ACCESSIBILITE-SOINS-PREMIER-RECOURS__VALID", opacity: 1},
+                //{title:"Zonage CPTS", layername: "IGNF_CARTO-SP_PPR_ZONAGE-CPTS__VALID", opacity: 1},
             ]
         }
     ];
